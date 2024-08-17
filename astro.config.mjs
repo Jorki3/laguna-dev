3ab148265fdbf4920ade1b470093b37924590b2a
Eduardo Trevizo <50352230+Jorki3@users.noreply.github.com>
2024-08-17 19:01:44 +0000

    New form

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import alpinejs from "@astrojs/alpinejs";
+import { server } from "typescript";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs()]
+  integrations: [tailwind(), alpinejs()],
+  experimental: {
+    actions: true,
+  },
+  output: "server",
+  adapter: netlify(),
 });
---
+++ src/actions/index.ts
@@ -0,0 +1,5 @@
+import { createAccount } from "./createAccount";
+
+export const server = {
+  createAccount: createAccount,
+};
