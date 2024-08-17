a690e1bb201340c5644b592758865f67d1bda648
Jorki3 <eduardomttzt01@outlook.com>
2024-08-17 02:28:52 -0600

    Changin config

---
+++ src/firebase/client.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBi1YW6gtbnuNnyPh9Mh3wNjeOiDg5nY2Q",
+  authDomain: "laguna-dev-80c74.firebaseapp.com",
+  projectId: "laguna-dev-80c74",
+  storageBucket: "laguna-dev-80c74.appspot.com",
+  messagingSenderId: "1031722419256",
+  appId: "1:1031722419256:web:c0431ac713496ea7de92cd",
+};
+
+export const app = initializeApp(firebaseConfig);
---
+++ src/firebase/server.ts
@@ -0,0 +1,30 @@
+import type { ServiceAccount } from "firebase-admin";
+import { initializeApp, cert, getApps } from "firebase-admin/app";
+
+const activeApps = getApps();
+const serviceAccount = {
+  type: "service_account",
+  project_id: import.meta.env.FIREBASE_PROJECT_ID,
+  private_key_id: import.meta.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: import.meta.env.FIREBASE_PRIVATE_KEY,
+  client_email: import.meta.env.FIREBASE_CLIENT_EMAIL,
+  client_id: import.meta.env.FIREBASE_CLIENT_ID,
+  auth_uri: import.meta.env.FIREBASE_AUTH_URI,
+  token_uri: import.meta.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: import.meta.env.FIREBASE_AUTH_CERT_URL,
+  client_x509_cert_url: import.meta.env.FIREBASE_CLIENT_CERT_URL,
+};
+
+const initApp = () => {
+  if (import.meta.env.PROD) {
+    console.info("PROD env detected. Using default service account.");
+    // Usa la configuración predeterminada en las funciones de Firebase. Debería estar ya inyectada en el servidor por Firebase.
+    return initializeApp();
+  }
+  console.info("Loading service account from env.");
+  return initializeApp({
+    credential: cert(serviceAccount as ServiceAccount),
+  });
+};
+
+export const app = activeApps.length === 0 ? initApp() : activeApps[0];
