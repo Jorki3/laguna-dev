6c214d4388a2d23a986634c13c0f5a049282a40f
Jorki3 <eduardomttzt01@outlook.com>
2024-08-18 15:20:40 -0600

    Another changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import alpinejs from "@astrojs/alpinejs";
-import { server } from "typescript";
 
 // https://astro.build/config
 export default defineConfig({
