a65c1813068b1a0c1f001f4577fe389a53a0a136
Jorki3 <eduardomttzt01@outlook.com>
2024-08-18 17:27:49 -0600

    Adding netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import alpinejs from "@astrojs/alpinejs";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), alpinejs()],
   output: "server",
+  adapter: netlify(),
 });
