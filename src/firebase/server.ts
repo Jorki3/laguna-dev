fe45b48ad141132d12d5c7e2949d852a23156f3c
Jorki3 <eduardomttzt01@outlook.com>
2024-08-18 17:42:10 -0600

    Privatekey replace

--- src/firebase/server.ts
+++ src/firebase/server.ts
@@ -6,7 +6,7 @@ const serviceAccount = {
   type: "service_account",
   project_id: import.meta.env.FIREBASE_PROJECT_ID,
   private_key_id: import.meta.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: import.meta.env.FIREBASE_PRIVATE_KEY,
+  private_key: import.meta.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
   client_email: import.meta.env.FIREBASE_CLIENT_EMAIL,
   client_id: import.meta.env.FIREBASE_CLIENT_ID,
   auth_uri: import.meta.env.FIREBASE_AUTH_URI,
