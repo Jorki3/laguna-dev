de0b72b4fc134bb0bec2e5acc3665023ed015bce
Jorki3 <eduardomttzt01@outlook.com>
2024-08-18 17:53:05 -0600

    process en evz d eimport.meta

--- src/firebase/server.ts
+++ src/firebase/server.ts
@@ -4,15 +4,15 @@ import { initializeApp, cert, getApps } from "firebase-admin/app";
 const activeApps = getApps();
 const serviceAccount = {
   type: "service_account",
-  project_id: import.meta.env.FIREBASE_PROJECT_ID,
-  private_key_id: import.meta.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: import.meta.env.FIREBASE_PRIVATE_KEY,
-  client_email: import.meta.env.FIREBASE_CLIENT_EMAIL,
-  client_id: import.meta.env.FIREBASE_CLIENT_ID,
-  auth_uri: import.meta.env.FIREBASE_AUTH_URI,
-  token_uri: import.meta.env.FIREBASE_TOKEN_URI,
-  auth_provider_x509_cert_url: import.meta.env.FIREBASE_AUTH_CERT_URL,
-  client_x509_cert_url: import.meta.env.FIREBASE_CLIENT_CERT_URL,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
 };
 
 const initApp = () => {
