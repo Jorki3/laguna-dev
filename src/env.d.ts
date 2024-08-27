872a594b24ce2cf6d8e71fbff6900e85422723aa
Jorki3 <eduardomttzt01@outlook.com>
2024-08-26 23:01:59 -0600

    Change on process

--- src/env.d.ts
+++ src/env.d.ts
@@ -10,11 +10,6 @@ interface ImportMetaEnv {
   readonly FIREBASE_TOKEN_URI: string;
   readonly FIREBASE_AUTH_CERT_URL: string;
   readonly FIREBASE_CLIENT_CERT_URL: string;
-  readonly FIREBASE_API_KEY: string;
-  readonly FIREBASE_AUTH_DOMAIN: string;
-  readonly FIREBASE_STORAGE_BUCKET: string;
-  readonly FIREBASE_MESSAGING_SENDER_ID: string;
-  readonly FIREBASE_APP_ID: string;
 }
 
 interface ImportMeta {
