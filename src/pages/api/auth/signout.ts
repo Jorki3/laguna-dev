bc29134f7271e214c98ef0d56c210cba823f4ac0
Jorki3 <eduardomttzt01@outlook.com>
2024-08-27 00:51:11 -0600

    Uninstalling Firebase

--- src/pages/api/auth/signout.ts
+++
@@ -1,8 +0,0 @@
-import type { APIRoute } from "astro";
-
-export const GET: APIRoute = async ({ redirect, cookies }) => {
-  cookies.delete("__session", {
-    path: "/",
-  });
-  return redirect("/signin");
-};
